837241fc3e5142ec9a9ad8ec147d1d2b8524db40
TiNezlobinsky <nezlobinsky@gmail.com>
2026-02-17 14:34:49 +0100

    compatibility namespace for __init__

--- finitewave/__init__.py
+++ finitewave/__init__.py
@@ -71,3 +71,56 @@ from finitewave.tools import (
     Velocity2DCalculation,
     Velocity3DCalculation,
 )
+
+
+# compatibility with older versions:
+
+CardiacTissue2D = CardiacTissue
+CardiacTissue3D = CardiacTissue
+
+AlievPanfilov2D = AlievPanfilov
+AlievPanfilov3D = AlievPanfilov
+Barkley2D = Barkley
+Barkley3D = Barkley
+MitchellSchaeffer2D = MitchellSchaeffer
+MitchellSchaeffer3D = MitchellSchaeffer
+FentonKarma2D = FentonKarma
+FentonKarma3D = FentonKarma
+BuenoOrovio2D = BuenoOrovio
+BuenoOrovio3D = BuenoOrovio
+LuoRudy912D = LuoRudy91
+LuoRudy913D = LuoRudy91
+TP062D = TenTusscherPanfilov2006
+TP063D = TenTusscherPanfilov2006
+Courtemanche2D = Courtemanche
+Courtemanche3D = Courtemanche
+
+StimCurrentArea2D = StimCurrentArea
+StimCurrentArea3D = StimCurrentArea
+StimCurrentCoord2D = StimCurrentCoord
+StimCurrentCoord3D = StimCurrentCoord
+StimVoltageCoord2D = StimVoltageCoord
+StimVoltageCoord3D = StimVoltageCoord
+StimCurrentMatrix2D = StimCurrentMatrix
+StimCurrentMatrix3D = StimCurrentMatrix
+StimVoltageMatrix2D = StimVoltageMatrix
+StimVoltageMatrix3D = StimVoltageMatrix
+
+ActionPotential2DTracker = ActionPotentialTracker
+ActionPotential3DTracker = ActionPotentialTracker
+ActivationTime2DTracker = ActivationTimeTracker
+ActivationTime3DTracker = ActivationTimeTracker
+Animation2DTracker = AnimationTracker
+Animation3DTracker = AnimationTracker
+ECG2DTracker = ECGTracker
+ECG3DTracker = ECGTracker
+LocalActivationTime2DTracker = LocalActivationTimeTracker
+LocalActivationTime3DTracker = LocalActivationTimeTracker
+Period2DTracker = PeriodTracker
+Period3DTracker = PeriodTracker
+SpiralWaveCore2DTracker = SpiralWaveCoreTracker
+SpiralWaveCore3DTracker = SpiralWaveCoreTracker
+Variables2DTracker = VariablesTracker
+Variables3DTracker = VariablesTracker
+MultiVariable2DTracker = VariablesTracker
+MultiVariable3DTracker = VariablesTracker
